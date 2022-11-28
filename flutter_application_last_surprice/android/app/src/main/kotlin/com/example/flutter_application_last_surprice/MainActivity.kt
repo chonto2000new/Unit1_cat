a9545b7617b22fb20dc1bb5c0d708b127851bddb
AGUSTIN ALBERTO  MONARREZ HERRERA <agustin.monarrez.is@unipolidgo.edu.mx>
2022-11-28 10:58:07 -0600

    Unit1_cat_2
    Update

---
+++ flutter_application_last_surprice/android/app/src/main/kotlin/com/example/flutter_application_last_surprice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_last_surprice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- the_shop/android/app/src/main/kotlin/com/example/the_shop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.the_shop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
